b93f1190966257568c4cbe216944eb9a0179c79e
amitrov <andela.mitrovic.24@um.edu.mt>
2024-11-08 21:56:37 +0100

    Initial commit with operation  program

---
+++ build/CMakeFiles/Operation.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Operation.dir/operation.c.o"
+  "CMakeFiles/Operation.dir/operation.c.o.d"
+  "Operation"
+  "Operation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Operation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ operation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int input;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &input);
+
+    
+    printf("The corresponding character is: %c\n", (char)input);
+
+    return 0;
+}
